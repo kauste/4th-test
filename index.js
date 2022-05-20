fdaf9fe374a727fbda6a882d70109aded1138c9f
Rugile <rugilestasionyte@gmail.com>
2022-05-20 10:30:55 +0300

    week-9

---
+++ index.js
@@ -0,0 +1,78 @@
+//const africa = ['Zebras', 'Liūtas',  '', 'Raganosis', '','Raganosis', 'Begemotas'];
+const australia = ['Kengūra', 'Ančiasnapis', 'Dingo', 'Atsirado', 'Strutis'];
+
+function rand(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+
+// 1 uzduotis
+const h2DOM = document.querySelectorAll('h2');
+
+for (i = 0; i < h2DOM.length; i++){
+    $randNum = rand(1, 6);
+    h2DOM[i].innerHTML += `${$randNum}`;
+}
+
+const buttonDOM = document.querySelector('button');
+
+function changeRand(){
+    for (i = 0; i < h2DOM.length; i++){
+        $randNum = rand(1, 6);
+        h2DOM[i].innerHTML = `${$randNum}`;
+    }
+    if(h2DOM[0].innerHTML == h2DOM[1].innerHTML){
+        for (i = 0; i < h2DOM.length; i++){
+            h2DOM[i].style.color = 'red';
+        }
+    }
+}
+
+buttonDOM.addEventListener('click', changeRand);
+
+// 2 uzduotis
+
+const numbArray = [];
+
+const button2DOM = document.querySelector('.button2');
+
+function pushNumb (){
+    let randNum = rand(1, 10);
+    numbArray.push(randNum);
+    console.log(numbArray);
+}
+button2DOM.addEventListener('click', pushNumb);
+
+const h3DOM = document.querySelector('h3');
+
+let sum = 0;
+
+function sumH3(){
+    for(let i = 0; i < numbArray.length; i++){
+        sum += numbArray[i];
+    }
+    h3DOM.innerHTML = `${sum}`;
+}
+
+button2DOM.addEventListener('click', sumH3);
+
+// 3 uzduotis
+ulDOM = document.querySelector('ul');
+
+for(let i = 0; i < australia.length; i++){
+    console.log(australia[i]);
+    let australiaList = document.createElement('li');
+        australiaList.innerHTML = `${australia[i]}`;
+    console.log(australiaList);
+    ulDOM.innerHTML += australiaList.innerHTML;
+}
+
+
+
+
+
+
+
+
